7137dd98d2ad7eab1d4dcec284de913123e7faed
gagai <gagai.vlad@gmail.com>
2022-11-30 20:07:28 +0300

    remove imports

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import get_object_or_404, render
 from recipes.models import Tag
 from rest_framework import viewsets
-from rest_framework.response import Response
 
 from .serializers import TagSerializer
 
