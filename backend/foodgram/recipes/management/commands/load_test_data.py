7da2355fb94ec65f21e5bd630eac1bf6cbcf2019
gagai <gagai.vlad@gmail.com>
2022-11-30 21:14:55 +0300

    add load_test_data

---
+++ backend/foodgram/recipes/management/commands/load_test_data.py
@@ -0,0 +1,21 @@
+import csv
+
+from django.core.management.base import BaseCommand, CommandError
+from foodgram.settings import BASE_DIR
+from recipes.models import Tag
+
+file_model_dict = {
+    'tags.csv': Tag,
+}
+path = BASE_DIR + '../data/'
+
+
+class Command(BaseCommand):
+    help = 'Загружает тестовые данные в бд'
+
+    def handle(self, *args, **kwargs):
+        for file, model in file_model_dict.items():
+            with open(f'{path}{file}') as file:
+                reader = csv.DictReader(file, delimiter=',')
+                for data in reader:
+                    model.objects.create(**data)
