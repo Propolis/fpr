c8f0e7a8019f2af3dd882b7eebc17b4c680de4ad
gagai <gagai.vlad@gmail.com>
2022-12-04 22:12:28 +0300

    add CORS

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'colorfield',
     'recipes',
     'users',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -144,3 +146,12 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+
+# CORS Settings
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost/'
+]
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_URL_REGEX = r'^/api/.*$'
