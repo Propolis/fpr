3abdc1294a91acde0932ea2ece28dec68fa50c7a
gagai <gagai.vlad@gmail.com>
2022-12-05 00:03:27 +0300

    remove simpleJWT settings

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -153,10 +153,10 @@ REST_FRAMEWORK = {
     ]
 }
 
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
-    'AUTH_HEADER_TYPES': ('Bearer',)
-}
+# SIMPLE_JWT = {
+#     'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+#     'AUTH_HEADER_TYPES': ('Bearer',)
+# }
 
 
 # CORS Settings
