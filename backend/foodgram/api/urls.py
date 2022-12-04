99ed48ef22b5e7701f092682e642987c0ecd716a
gagai <gagai.vlad@gmail.com>
2022-12-05 00:00:32 +0300

    add djoser endpoints

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,3 +1,4 @@
+from django.urls import include, path
 from rest_framework import routers
 
 from .views import TagViewSet
@@ -5,3 +6,6 @@ from .views import TagViewSet
 router = routers.SimpleRouter()
 router.register('tags', TagViewSet)
 urlpatterns = router.urls
+urlpatterns += [
+    path('auth/', include('djoser.urls')),
+]
