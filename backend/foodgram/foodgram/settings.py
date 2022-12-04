a55342fb2b141082ea3d30acc2cf375bed68ecc1
gagai <gagai.vlad@gmail.com>
2022-12-04 23:16:37 +0300

    add drf auth

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -143,7 +143,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
@@ -151,7 +154,7 @@ REST_FRAMEWORK = {
 # CORS Settings
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost/'
+    'http://localhost'
 ]
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_URL_REGEX = r'^/api/.*$'
