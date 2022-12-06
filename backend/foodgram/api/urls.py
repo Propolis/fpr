b431df4c7057a267836250e16c0b557cd69f5ea8
gagai <gagai.vlad@gmail.com>
2022-12-06 17:54:24 +0300

    change simple router to default

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,11 +1,9 @@
 from django.urls import include, path
 from rest_framework import routers
 
+
 from .views import TagViewSet
 
-router = routers.SimpleRouter()
+router = routers.DefaultRouter()
 router.register('tags', TagViewSet)
 urlpatterns = router.urls
-urlpatterns += [
-    path('auth/', include('djoser.urls')),
-]
