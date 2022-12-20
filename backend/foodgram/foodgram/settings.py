0482ed88e3b7832e75a39e7f0d34cef4ed32944b
gagai <gagai.vlad@gmail.com>
2022-12-20 23:22:24 +0300

    change dotenv settings

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -6,8 +6,7 @@ from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-dotenv_path = Path(BASE_DIR / '../infra/.env')
-load_dotenv(dotenv_path=dotenv_path)
+load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
