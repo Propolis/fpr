d1d6246080eba1d61d217045273ca2c83e938171
gagai <gagai.vlad@gmail.com>
2022-12-22 17:41:34 +0300

    models class meta, add some admin page features

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-from .models import Ingredient, Recipe, RecipeIngredient, RecipeTag, Tag
+from .models import (
+    FavoriteRecipe,
+    Ingredient,
+    Recipe,
+    RecipeIngredient,
+    RecipeTag,
+    ShoppingCart,
+    Tag
+)
 
 
 class RecipeIngredientInline(admin.TabularInline):
@@ -17,18 +26,43 @@ class RecipeTagInline(admin.TabularInline):
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit')
     list_filter = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'author')
-    list_filter = ('name', 'author', 'tags')
+    list_display = ('name', 'author', 'count_favorites')
+    list_filter = ('name', 'author__email', 'tags')
     inlines = [
         RecipeIngredientInline,
         RecipeTagInline
     ]
 
+    @admin.display(description='В избранном')
+    def count_favorites(self, obj):
+        return FavoriteRecipe.objects.filter(recipe=obj).count()
 
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
+
+@admin.register(FavoriteRecipe)
+class FavoriteRecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientCartAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RecipeTag)
+class RecipeTagAdmin(admin.ModelAdmin):
     pass
--- backend/users/models.py
+++ backend/users/models.py
@@ -3,10 +3,20 @@ from django.db import models
 
 
 class User(AbstractUser):
-    email = models.EmailField(unique=True, blank=False, max_length=254)
+    email = models.EmailField(
+        unique=True,
+        blank=False,
+        max_length=254,
+        verbose_name='Почта',
+        help_text='Введи свою почту!'
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
 
 class Subscription(models.Model):
